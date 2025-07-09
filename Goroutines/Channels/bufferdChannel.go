af940d19bf828e4b91a89ddd632b8ed2c5a758bf
karanhasan1 <kamranhasan255@gmail.com>
2025-07-09 11:04:25 +0530

    double channel

--- Goroutines/Channels/bufferdChannel.go
+++ Goroutines/Channels/bufferdChannel.go
@@ -12,12 +12,24 @@ func main(){
 	done := make(chan bool)
 	go emailSender(emailChan,done)
 
-	for i := 0 ; i<100; i++{
+	for i := 0 ; i<2; i++{
 		emailChan <- fmt.Sprint("%d@gmail.com",i)
 	}
 	fmt.Println("done sending")
+	//this is important to close 
+	close(emailChan)
 	 
 	<-done
+	
+	//double channel 
+	chan1 := make(chan string)
+	chan2 := make(chan int)
+	go data(chan1,chan2)
+	time.Sleep(time.Second*1)
+	
+
+
+
 } 
 
 
@@ -30,4 +42,26 @@ func emailSender(emailChan chan string,done chan bool){
 		fmt.Println("sending email to ",email)
 		time.Sleep(time.Second *1)
 	}
+}
+
+//double channel 
+func data(chan1 chan string, chan2 chan int ){
+ 
+	go func ()  {
+		chan1<-"ping pong "
+		
+	}()
+	go func ()  {
+		chan2<- 14
+	}()
+
+	for i:= 0; i<2; i++{
+	select{
+	case chan1Val:=<-chan1:
+		fmt.Println("recived data from chan1",chan1Val)
+	case chan2Val := <-chan2:
+		fmt.Println("he was saying me ",chan2Val)
+	}	
+	}
+
 }
